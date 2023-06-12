1cb20b7f23a4348cf0d9db6e21ab84275dec44f1
Anitha chowdary <130220172+anitha-anii@users.noreply.github.com>
2023-06-12 13:57:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-//your JS code here. If required.
+document.addEventListener('DOMContentLoaded', () => {
+  let obtn = document.getElementById('open');
+  
+  obtn.addEventListener('click', () => {
+    let displayDiv = document.createElement('div');
+    displayDiv.className = 'ddiv';
+
+    let header = document.createElement('header');
+    header.innerText = "Model Header";
+    let tbody = document.createElement('tbody');
+    let para = document.createElement('p');
+    para.textContent = "Model Body Content";
+    let cbutton = document.createElement('button');
+    cbutton.className = 'close';
+    cbutton.innerHTML = "X";
+
+    tbody.appendChild(para);
+    tbody.appendChild(cbutton);
+    displayDiv.appendChild(header);
+    displayDiv.appendChild(tbody);
+
+    document.body.appendChild(displayDiv);
+
+    cbutton.addEventListener('click', () => {
+      displayDiv.remove();
+    });
+  });
+});
+

a19a0a43b4b7ef9ca1fd9b7288d8d151c5d0af92
Anitha chowdary <130220172+anitha-anii@users.noreply.github.com>
2023-07-03 19:27:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,46 +1,21 @@
-document.addEventListener('DOMContentLoaded', () => {
-  let obtn = document.getElementById('openModal');
-  
-  obtn.addEventListener('click', () => {
-    let displayDiv = document.createElement('div');
-    displayDiv.className = 'modal';
+var modal = document.getElementById("modal");
+var openModalBtn = document.getElementById("openModalBtn");
+var closeModalBtn = document.getElementById("closeModalBtn");
 
-    let header = document.createElement('h2');
-    header.className = 'modal-header';
-    header.innerText = "Modal Header";
-    
-    let para = document.createElement('p');
-    para.className = 'modal-body';
-    para.textContent = "Modal Body Content";
-    
-    let cbutton = document.createElement('button');
-    cbutton.className = 'close-modal';
-    cbutton.innerHTML = "X";
-
-    let modalContent = document.createElement('div');
-    modalContent.className = 'modal-content';
-    
-    let modalHeader = document.createElement('div');
-    modalHeader.className = 'modal-header';
-    modalHeader.appendChild(header);
-    modalHeader.appendChild(cbutton);
-    
-    let modalBody = document.createElement('div');
-    modalBody.className = 'modal-body';
-    modalBody.appendChild(para);
-    
-    modalContent.appendChild(modalHeader);
-    modalContent.appendChild(modalBody);
-    
-    displayDiv.appendChild(modalContent);
-
-    document.body.appendChild(displayDiv);
-
-    cbutton.addEventListener('click', () => {
-      displayDiv.remove();
-    });
-  });
+openModalBtn.addEventListener("click", function() {
+  modal.style.display = "block";
 });
+
+closeModalBtn.addEventListener("click", function() {
+  modal.style.display = "none";
+});
+
+window.addEventListener("click", function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+});
+
 
 
 
